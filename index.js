8f49998575ec1de9fb2bd086855ef78347a98e3b
amine <jmalmohamedamine1@gmail.com>
2024-10-17 13:39:41 +0100

    first commit

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
-
+import express, { json } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
 var app = express();
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+dotenv.config();
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use(cors());
+app.use("/public", express.static(process.cwd() + "/public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const upload = multer();
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-
-
+app.post("/api/fileanalyse", upload.single("upfile"), function (req, res) {
+  res.json({
+    name: req.file.originalname,
+    size: req.file.size,
+    type: req.file.mimetype,
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
